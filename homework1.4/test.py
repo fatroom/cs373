e66d418548bb44c3cca1a055c5461ec22602c24d
Roman Romanchuk <roman.romanchuk@me.com>
2012-02-23 20:03:17 +0200

    Two more tests added

--- homework1.4/test.py
+++ homework1.4/test.py
@@ -18,5 +18,43 @@ class TestSequenceFunctions(unittest.TestCase):
 
     self.assertEqual(expected_result, p)
 
+  def test_dataset2(self):
+    expected_result=[[0,0,0],[0,0.5,0.5],[0,0,0]]
+
+    task.colors = [['green','green','green'],
+                   ['green','red','red'],
+                   ['green','green','green']]	
+    task.measurements=['red']
+    task.motions=[[0,0]]
+    task.sensor_right=1.0
+    task.p_move=1.0
+
+    p = task.calculate()
+
+    self.assertEqual(expected_result, p)
+
+  def test_dataset3(self):
+    expected_result=[[0.06666,0.06666, 0.06666],[0.06666,0.26666,0.26666],[0.06666,0.06666,0.06666]]
+
+    task.colors = [['green','green','green'],
+                   ['green','red','red'],
+                   ['green','green','green']]	
+    task.measurements=['red']
+    task.motions=[[0,0]]
+    task.sensor_right=0.8
+    task.p_move=1.0
+
+    p = task.calculate()
+
+    for i in range(len(p)):
+      for j in range(len(p[1])):
+        p[i][j] = round(p[i][j],4)
+
+    for i in range(len(expected_result)):
+      for j in range(len(expected_result[1])):
+        expected_result[i][j] = round(expected_result[i][j],4)
+
+    self.assertEqual(expected_result, p)
+
 if __name__ == '__main__':
   unittest.main()
