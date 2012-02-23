5eff90be19d758880df93d986c0e833eef7470be
Roman Romanchuk <roman.romanchuk@me.com>
2012-02-23 20:30:50 +0200

    New test case added

--- homework1.4/test.py
+++ homework1.4/test.py
@@ -98,5 +98,31 @@ class TestSequenceFunctions(unittest.TestCase):
 
     self.assertEqual(expected_result, p)
 
+  def test_dataset6(self):
+    expected_result=[[0.02898, 0.02898, 0.02898],
+                     [0.07246, 0.28985, 0.46376],
+                     [0.02898, 0.02898, 0.02898]]
+
+    task.colors = [['green', 'green', 'green'],
+                   ['green', 'red', 'red'],
+                   ['green', 'green', 'green']]	
+    task.measurements=['red', 'red']
+    task.motions=[[0,0], [0, 1]]
+    task.sensor_right=.8
+    task.p_move=0.5
+
+    p = task.calculate()
+
+    for i in range(len(p)):
+      for j in range(len(p[1])):
+        p[i][j] = round(p[i][j],4)
+
+    for i in range(len(expected_result)):
+      for j in range(len(expected_result[1])):
+        expected_result[i][j] = round(expected_result[i][j],4)
+
+    self.assertEqual(expected_result, p)
+
+ 
 if __name__ == '__main__':
   unittest.main()
